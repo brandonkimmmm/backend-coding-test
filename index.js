9a8b06339e643506d7d176bc6453b84cf917740d
Brandon Kim <bkim2490@gmail.com>
2021-12-04 18:00:48 +0900

    script updates for eslint rules

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-const express = require('express');
-const app = express();
 const port = 8010;
 const logger = require('./utils/logger');
-
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
@@ -18,11 +13,11 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./src/swagger.yaml');
 
 db.serialize(() => {
-    buildSchemas(db);
+	buildSchemas(db);
 
-    const app = require('./src/app')(db);
+	const app = require('./src/app')(db);
 
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-    app.listen(port, () => logger.info(`App started and listening on port ${port}`));
+	app.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
