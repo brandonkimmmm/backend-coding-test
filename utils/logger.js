8ca1821453c52398dc7df570aa1470577a906445
Brandon Kim <bkim2490@gmail.com>
2021-12-06 15:23:01 +0900

    Update logger format

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const winston = require('winston');
+const { SPLAT } = require('triple-beam');
 require('winston-daily-rotate-file');
+const { isObject } = require('lodash');
 
 const rotateTransport = new winston.transports.DailyRotateFile({
 	filename: 'rideapp-%DATE%.log',
@@ -12,11 +14,23 @@ const rotateTransport = new winston.transports.DailyRotateFile({
 	maxFiles: '14d'
 });
 
+const formatObject = (data) => isObject(data) ? JSON.stringify(data) : data;
+
+const myFormat = winston.format((info) => {
+	const splat = info[SPLAT] || [];
+	const message = formatObject(info.message);
+	const rest = splat.map(formatObject).join(' ');
+	info.message = `${message} ${rest}`;
+	return info;
+});
+
 const logger = winston.createLogger({
 	level: 'info',
 	format: winston.format.combine(
+		myFormat(),
 		winston.format.timestamp(),
-		winston.format.json()
+		winston.format.align(),
+		winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
 	),
 	transports: [
 		new winston.transports.Console(),
